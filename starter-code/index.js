13f31dd7173fc7a590ec30c9c4536e50426f255d
Raul Requena <raulrequec@gmail.com>
2019-10-22 20:49:17 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,49 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
-  
-  module.exports = SortedList;
+  constructor() {
+    this.items = [];
+  }
+
+  get length() {
+    return this.items.length;
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+  }
+
+  get(pos) {
+    if (this.items[pos] === undefined) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
+
+  max() {
+    if (this.length === 0)
+      throw new Error("EmptySortedList");
+    return this.items.slice(-1);
+  }
+
+  min() {
+    if (this.length === 0)
+      throw new Error("EmptySortedList");
+    return this.items.slice(0, 1);
+  }
+
+  average() {
+    if (this.length === 0)
+      throw new Error("EmptySortedList");
+    return this.sum() / this.length;
+  }
+
+  sum() {
+    if (this.length === 0) {
+      return 0;
+    } else {
+      return this.items.reduce((acc, item) => acc + item);
+    }
+  }
+}
+
+module.exports = SortedList;
